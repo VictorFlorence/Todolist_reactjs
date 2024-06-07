57449b7f262e54d366c43d288c00be8041d64337
VictorFlorence <vuhuy25092004@gmail.com>
2024-06-07 13:20:58 +0700

    Add folder react todolist

---
+++ src/components/TodoForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value) {
+      addTodo(value);
+      setValue('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder='TIÊU ĐỀ VIỆC CẦN LÀM ...'
+      />
+      <button type="submit" className='todo-btn'>Thêm mới</button>
+    </form>
+  );
+};
